d731018830149a49dab43cd2a8e0b8748b746e63
Jim McKeeth <jim@mckeeth.org>
2022-04-25 18:10:56 -0600

    A few UI changes

--- Lorenzfmx.dpr
+++ Lorenzfmx.dpr
@@ -3,12 +3,17 @@ program Lorenzfmx;
 uses
   System.StartUpCopy,
   FMX.Forms,
+  FMX.Types,
+  Skia.FMX,
   ufMain in 'ufMain.pas' {frmMain},
   uPlotPanel in 'uPlotPanel.pas';
 
 {$R *.res}
 
 begin
+  GlobalUseMetal := True;
+  GlobalUseSkiaRasterWhenAvailable := False;
+  GlobalUseSkia := True;
   Application.Initialize;
   Application.CreateForm(TfrmMain, frmMain);
   Application.Run;
